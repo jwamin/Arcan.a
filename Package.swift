ec3829d76016ad38d6eb0eabe151bbcdd0a4b858
Joss Manger <jossferatu@gmail.com>
2022-08-07 16:05:57 -0400

    refactors system module to systemLibrary

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
 // swift-tools-version:5.0
 import PackageDescription
 
-var includes: [Package.Dependency] = []
+var targetIncludes: [Target] = []
 var dependencies: [Target.Dependency] = []
 
 #if os(Linux)
-includes += [
-    .package(path: "./Sources/linux-lbsd-wrapper"),
+targetIncludes += [
+    .systemLibrary(name:"lbsd", path:"./Sources/linux-lbsd", pkgConfig:"libbsd")
 ]
 dependencies += [
-    "linux-lbsd-wrapper"
+    "lbsd"
 ]
 #endif
 
@@ -29,8 +29,8 @@ let package = Package(
         dependencies: [
             // Dependencies declare other packages that this package depends on.
             // .package(url: /* package url */, from: "1.0.0"),
-        ] + includes,
-        targets: [
+        ],
+        targets: targetIncludes + [
             // Targets are the basic building blocks of a package. A target can define a module or a test suite.
             // Targets can depend on other targets in this package, and on products in packages this package depends on.
             .target(
